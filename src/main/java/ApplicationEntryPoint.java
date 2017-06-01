d1c9ec4f2faddff2b3920dfe3b9a718816dfec76
jared <jrod23uhs@gmail.com>
2017-05-31 22:42:56 -0500

    ApplicationEntryPoint launches MainFormWrapper.  This 'seam' into the app is untested as it would require dependency injection, which app entry should stay simple

--- src/main/java/ApplicationEntryPoint.java
+++ src/main/java/ApplicationEntryPoint.java
@@ -1,6 +1,10 @@
+import gui.MainFormWrapper;
+
 public class ApplicationEntryPoint {
 
     public static void main(String[] args) {
         System.out.println("Launching Application Entry Point");
+        MainFormWrapper mainFormWrapper = new MainFormWrapper();
+        mainFormWrapper.launchForm();
     }
 }
