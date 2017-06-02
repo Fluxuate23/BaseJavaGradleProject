656c572525fd90d4632765c239638afabb1d6c59
jg08129 <greenjared@johndeere.com>
2017-06-01 22:40:17 -0500

    Refactored and reduced code by removing unneccessary if statement for inserting a penny, as it does not change the dollar amount for primitive double

--- src/main/java/brain/VendingMachineBrain.java
+++ src/main/java/brain/VendingMachineBrain.java
@@ -8,8 +8,6 @@ public class VendingMachineBrain {
     public void insertCoin(ECoin insertedCoin) {
         if (insertedCoin.getDiameterInMillimeters() == 21.21) {
             currentDollarAmount = .05;
-        } else if (insertedCoin.getDiameterInMillimeters() == 19.05){
-            currentDollarAmount = 0.0;
         } else if (insertedCoin.getDiameterInMillimeters() == 17.9){
             currentDollarAmount = 0.1;
         } else if (insertedCoin.getDiameterInMillimeters() == 24.26) {
