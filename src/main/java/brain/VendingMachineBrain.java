64a4b5694941675ec7e288fe6ebc34e3428c0e76
jg08129 <greenjared@johndeere.com>
2017-06-01 22:53:25 -0500

    Refactored VendingMachineBrain to use Dollar Values from CoinConstants

--- src/main/java/brain/VendingMachineBrain.java
+++ src/main/java/brain/VendingMachineBrain.java
@@ -2,16 +2,20 @@ package brain;
 
 import enums.ECoin;
 
+import static constants.CoinConstants.DOLLAR_VALUE_OF_DIME;
+import static constants.CoinConstants.DOLLAR_VALUE_OF_NICKLE;
+import static constants.CoinConstants.DOLLAR_VALUE_OF_QUARTER;
+
 public class VendingMachineBrain {
     private double currentDollarAmount;
 
     public void insertCoin(ECoin insertedCoin) {
         if (insertedCoin.getDiameterInMillimeters() == 21.21) {
-            currentDollarAmount = .05;
+            currentDollarAmount = DOLLAR_VALUE_OF_NICKLE;
         } else if (insertedCoin.getDiameterInMillimeters() == 17.9){
-            currentDollarAmount = 0.1;
+            currentDollarAmount = DOLLAR_VALUE_OF_DIME;
         } else if (insertedCoin.getDiameterInMillimeters() == 24.26) {
-            currentDollarAmount = 0.25;
+            currentDollarAmount = DOLLAR_VALUE_OF_QUARTER;
         }
     }
 
