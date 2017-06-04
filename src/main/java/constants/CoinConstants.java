18df99cd62d1aa300794fb63d2514faecd324dcd
jared <jrod23uhs@gmail.com>
2017-06-04 14:36:35 -0500

    Added CoinContstant.DOLLAR_VALUE_OF_PENNY for using in VendingMachineBrain coin return

--- src/main/java/constants/CoinConstants.java
+++ src/main/java/constants/CoinConstants.java
@@ -2,24 +2,25 @@ package constants;
 
 public class CoinConstants {
 
-//    Dollar value
+    //    Dollar value
+    public static final double DOLLAR_VALUE_OF_PENNY = 0.01;
     public static final double DOLLAR_VALUE_OF_NICKLE = 0.05;
     public static final double DOLLAR_VALUE_OF_DIME = 0.1;
     public static final double DOLLAR_VALUE_OF_QUARTER = 0.25;
 
-//    Weight in grams
+    //    Weight in grams
     public static final double WEIGHT_IN_GRAMS_OF_PENNY = 2.5;
     public static final double WEIGHT_IN_GRAMS_OF_NICKLE = 5.0;
     public static final double WEIGHT_IN_GRAMS_OF_DIME = 2.268;
     public static final double WEIGHT_IN_GRAMS_OF_QUARTER = 5.67;
 
-//    Diameter in millimeters
+    //    Diameter in millimeters
     public static final double DIAMETER_IN_MILLIMETERS_OF_PENNY = 19.05;
     public static final double DIAMETER_IN_MILLIMETERS_OF_NICKLE = 21.21;
     public static final double DIAMETER_IN_MILLIMETERS_OF_DIME = 17.9;
     public static final double DIAMETER_IN_MILLIMETERS_OF_QUARTER = 24.26;
 
-//    Thickness in millimeters
+    //    Thickness in millimeters
     public static final double THICKNESS_MILLIMETERS_OF_PENNY = 1.52;
     public static final double THICKNESS_MILLIMETERS_OF_NICKLE = 1.95;
     public static final double THICKNESS_MILLIMETERS_OF_DIME = 1.35;
--- src/test/java/constants/CoinConstantsTest.java
+++ src/test/java/constants/CoinConstantsTest.java
@@ -9,6 +9,7 @@ public class CoinConstantsTest {
 
     @Test
     public void coinConstantsHasCorrectDollarValueForNickleDimeAndQuarter() {
+        assertThat(CoinConstants.DOLLAR_VALUE_OF_PENNY, is(0.01));
         assertThat(CoinConstants.DOLLAR_VALUE_OF_NICKLE, is(0.05));
         assertThat(CoinConstants.DOLLAR_VALUE_OF_DIME, is(0.1));
         assertThat(CoinConstants.DOLLAR_VALUE_OF_QUARTER, is(0.25));
