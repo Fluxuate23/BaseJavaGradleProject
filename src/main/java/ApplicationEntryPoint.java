1de8d91760ad2b3fe7826a5da857ab1987f279ca
jared <jrod23uhs@gmail.com>
2017-06-13 17:33:21 -0500

    Deleted all classes and files not needed for a base java project

--- src/main/java/ApplicationEntryPoint.java
+++ src/main/java/ApplicationEntryPoint.java
@@ -1,10 +1,6 @@
-import gui.MainFormWrapper;
-
 public class ApplicationEntryPoint {
 
     public static void main(String[] args) {
         System.out.println("Launching Application Entry Point");
-        MainFormWrapper mainFormWrapper = new MainFormWrapper();
-        mainFormWrapper.launchForm();
     }
 }
